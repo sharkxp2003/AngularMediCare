1afa532149dacf93c7e40136aa20785a02890d82
alexCortizoFranza <alejandro.cortizo@udc.es>
2017-11-28 02:03:49 -0600

    Initial commit AngularMediCare

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name webstormProjectsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the webstormProjectsApp
+ */
+angular.module('webstormProjectsApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
